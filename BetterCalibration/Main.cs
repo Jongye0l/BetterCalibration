bedaf39496e749be4bb051f6d8963d051c602950
Jongyeol <bcpjy1233@gmail.com>
2024-09-23 19:32:00 +0900

    feat: (1.1.3) Change offset on Mod Menu

--- BetterCalibration/Main.cs
+++ BetterCalibration/Main.cs
@@ -1,4 +1,5 @@
-﻿using BetterCalibration.Features;
+﻿using System;
+using BetterCalibration.Features;
 using JALib.Core;
 using JALib.Tools;
 using UnityEngine;
@@ -9,6 +10,7 @@ namespace BetterCalibration;
 public class Main : JAMod {
     public static Main Instance;
     public static SettingGUI SettingGUI;
+    private static string offsetString;
 
     private Main(UnityModManager.ModEntry modEntry) : base(modEntry, true, gid: 1929334982) {
         Instance = this;
@@ -28,6 +30,38 @@ public class Main : JAMod {
         GUILayout.FlexibleSpace();
         GUILayout.EndHorizontal();
     }
+
+    protected override void OnGUIBehind() {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label(Localization["InputOffset"]);
+        GUILayout.Space(4f);
+        if(GUILayout.Button("-", GUILayout.Width(25))) {
+            scrConductor.currentPreset.inputOffset--;
+            scrConductor.SaveCurrentPreset();
+        }
+        int offset = scrConductor.currentPreset.inputOffset;
+        if(offsetString.IsNullOrEmpty() || !int.TryParse(offsetString, out int i) || i != offset) offsetString = offset.ToString();
+        offsetString = GUILayout.TextField(offsetString);
+        int resultInt;
+        try {
+            resultInt = offsetString.IsNullOrEmpty() ? offset : int.TryParse(offsetString, out i) ? i : offset;
+        } catch (FormatException) {
+            resultInt = offset;
+        }
+        if(resultInt != offset) {
+            scrConductor.currentPreset.inputOffset = resultInt;
+            scrConductor.SaveCurrentPreset();
+        }
+        GUILayout.Label("ms");
+        if(GUILayout.Button("+", GUILayout.Width(25))) {
+            scrConductor.currentPreset.inputOffset++;
+            scrConductor.SaveCurrentPreset();
+        }
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+    }
+
+    protected override void OnHideGUI() => offsetString = null;
 
     private void AddLanguageButton(string text, SystemLanguage? lang) {
         if(!GUILayout.Button(text)) return;
