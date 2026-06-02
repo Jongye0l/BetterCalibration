e90f7c6d8d6d140f3b081ec59a2ac231d84dd882
Jongyeol <bcpjy1233@gmail.com>
2026-06-03 02:00:40 +0900

    fix: detail is not work in r141

--- BetterCalibration/Features/CalibrationDetail.cs
+++ BetterCalibration/Features/CalibrationDetail.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using JALib.Core;
 using JALib.Core.Patch;
+using JALib.Tools;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,7 +11,7 @@ namespace BetterCalibration.Features;
 
 public class CalibrationDetail() : Feature(Main.Instance, nameof(CalibrationDetail), true, typeof(CalibrationDetail)) {
     private static Text _text;
-    private static List<double> _timings;
+    private static IList _timings;
     private static float? _max;
     private static float? _min;
 
@@ -26,7 +28,7 @@ public class CalibrationDetail() : Feature(Main.Instance, nameof(CalibrationDeta
     
     [JAPatch("scrCalibrationPlanet", "Start", PatchType.Postfix, true, MaxVersion = 140)]
     [JAPatch(nameof(scnCalibration), "Start", PatchType.Postfix, true, MinVersion = 141)]
-    public static void Initialize(Text ___txtResults, List<double> ___listOffsets) {
+    public static void Initialize(Text ___txtResults, IList ___listOffsets) {
         _text = ___txtResults;
         _timings = ___listOffsets;
     }
@@ -41,9 +43,13 @@ public class CalibrationDetail() : Feature(Main.Instance, nameof(CalibrationDeta
     }
 
     [JAPatch("scrCalibrationPlanet", "PutDataPoint", PatchType.Postfix, false, MaxVersion = 140)]
-    [JAPatch(nameof(scnCalibration), "PutDataPoint", PatchType.Postfix, false, MinVersion = 141)]
-    public static void ReloadText() {
+    public static void ReloadTextR136() {
         if(_text) _text.text = string.Format(Main.Instance.Localization.Get("Cablibration.Detail"), ToStringAuto(GetTimingAverage()), ToStringAuto(_max ?? 0), ToStringAuto(_min ?? 0));
+    }
+
+    [JAPatch(nameof(scnCalibration), "PutDataPoint", PatchType.Postfix, false, MinVersion = 141)]
+    public static void ReloadTextR141(bool ___calibrated) {
+        if(!___calibrated && _text) _text.text = string.Format(Main.Instance.Localization.Get("Cablibration.Detail"), ToStringAuto(GetTimingAverage()), ToStringAuto(_max ?? 0), ToStringAuto(_min ?? 0));
     }
 
     [JAPatch("scrCalibrationPlanet", "SetMessageNumber", PatchType.Postfix, false, MaxVersion = 140)]
@@ -59,7 +65,15 @@ public class CalibrationDetail() : Feature(Main.Instance, nameof(CalibrationDeta
         return FloatOffset.Instance.Enabled ? f.ToString("0.##") : Mathf.RoundToInt(f).ToString();
     }
 
-    private static float GetTimingAverage() {
-        return _timings.Count == 0 ? 0f : (float) (_timings.Sum() / _timings.Count) * 1000;
+    private static float GetTimingAverage() => VersionControl.releaseNumber < 141 ? GetTimingAverageR136() : GetTimingAverageR141();
+
+    private static float GetTimingAverageR136() {
+        List<double> timings = _timings.AsUnsafe<List<double>>();
+        return timings.Count == 0 ? 0f : (float) (timings.Sum() / timings.Count) * 1000;
+    }
+
+    private static float GetTimingAverageR141() {
+        List<scnCalibration.OffsetPair> timings = _timings.AsUnsafe<List<scnCalibration.OffsetPair>>();
+        return timings.Count == 0 ? 0f : (float) (timings.Sum(t => t.offset) / timings.Count) * 1000;
     }
 }
